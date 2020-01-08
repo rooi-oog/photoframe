fd5f9aa37ad121799258cb559b9b1208929c8b80
Alex Petrov <a.y.p@inbox.ru>
2020-01-09 03:02:30 +0600

    cleanup

--- software/common/hw/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-#include <stdint.h>
-
-#define MMPTR(x)					(*((volatile uint32_t *) x))
-
-#endif /* COMMON_H */
-
--- software/common/hw/interrupts.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HW_INTERRUPTS_H
-#define __HW_INTERRUPTS_H
-
-#define IRQ_UART			(0x00000001) /* 0 */
-#define IRQ_GPIO			(0x00000002) /* 1 */
-#define IRQ_TIMER0			(0x00000004) /* 2 */
-#define IRQ_TIMER1			(0x00000008) /* 3 */
-
-#endif /* __HW_INTERRUPTS_H */
